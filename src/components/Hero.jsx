47286e6dce8ba97823659295bf7b691f6769ac87
praju120056 <praju2005x86@gmail.com>
2026-04-26 02:02:24 +0530

    Updated working PHOTO_URL

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -10,7 +10,7 @@ const fadeUp = {
 };
 
 const PHOTO_URL =
-  'https://drive.google.com/uc?export=view&id=1v2QKtO6LuxYx6zw_6EX_KV7yNyu1skxH';
+  "https://drive.google.com/thumbnail?id=17ud0aHQ1nhnht1-Q5hHrLUKyqOrCxVZF&sz=w600";
 
 export default function Hero() {
   return (
