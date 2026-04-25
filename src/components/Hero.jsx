01491f5c7abdb02813aeb716936bb9ed1c6dc71a
Prajakth N Kumar <135407160+praju120056@users.noreply.github.com>
2026-04-26 01:41:07 +0530

    Update PHOTO_URL to new Google Drive link

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -10,7 +10,7 @@ const fadeUp = {
 };
 
 const PHOTO_URL =
-  'https://drive.google.com/file/d/1rcnQ6_VKk2TwqanbKcvG5fZeolp1Y4xX/view?usp=drivesdk';
+  'https://drive.google.com/file/d/1v2QKtO6LuxYx6zw_6EX_KV7yNyu1skxH/view?usp=sharing';
 
 export default function Hero() {
   return (
